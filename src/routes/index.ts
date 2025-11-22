e022a1e084f29323154b94634a909d76492d6ac2
JcT <jc.torrel@normandiewebschool.fr>
2025-11-22 11:43:01 +0100

    Removing /programs route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,12 @@
 
 import express from "express";
 import createHealthRouter from "./health";
-import createProgramsRouter from "./programs";
 import createChatRouter from "./chat";
 
 export default function createApiRouter(args: any) {
     const router = express.Router();
 
     router.use("/api", createHealthRouter(args.openai));
-    router.use("/api", createProgramsRouter());
     router.use("/api", createChatRouter(args));
 
     return router;
