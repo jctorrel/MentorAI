3e1f4542e3c166f6f517985dc474dc30eda7cec1
JcT <jc.torrel@normandiewebschool.fr>
2025-11-22 09:48:25 +0100

    New route : /api/programs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 
 import express from "express";
 import createHealthRouter from "./health";
+import createProgramsRouter from "./programs";
 import createChatRouter from "./chat";
 
 export default function createApiRouter(args: any) {
     const router = express.Router();
 
     router.use("/api", createHealthRouter());
+    router.use("/api", createProgramsRouter());
     router.use("/api", createChatRouter(args));
 
     return router;
